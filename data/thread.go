5ed75220624aebe66efe38726787bba6ce3c23c6
sasshi-i <i.satoshi.0915@gmail.com>
2019-11-15 23:20:36 +0900

    Add Threads, NumReplies method

--- data/thread.go
+++ data/thread.go
@@ -11,3 +11,33 @@ type Thread struct {
 	UserId    int
 	CreatedAt time.Time
 }
+
+func Threads() (threads []Thread, err error) {
+	rows, err := Db.Query("SELECT id, uuid, topic, user_id, created_at FROM threads ORDER BY created_at DESC")
+	if err != nil {
+		return
+	}
+	for rows.Next() {
+		th := Thread{}
+		if err = rows.Scan(&th.Id, &th.Uuid, &th.Topic, &th.UserId, &th.CreatedAt); err != nil {
+			return
+		}
+		threads = append(threads, th)
+	}
+	rows.Close()
+	return
+}
+
+func (thread *Thread) NumReplies() (count int) {
+	rows, err := Db.Query("SELECT COUNT(*) FROM posts where thread_id = $1, thread.Id")
+	if err != nil {
+		return
+	}
+	for rows.Next() {
+		if err = rows.Scan(&count); err != nil {
+			return
+		}
+	}
+	rows.Close()
+	return
+}
